2a96c218670f0c58350e6382190911c14cf10262
AV-01 <aryavratmishra007@gmail.com>
2024-03-08 20:04:01 -0800

    Added sign up page
    Still needs some server end stuff in order to work

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('home.html')
+
+@app.route('/sign-up')
+def sign_up():
+    return render_template('sign-up.html')
+
+@app.route('/create_account')
+def create_account():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    email = request.args.get('email')
+    try:
+        os.mkdir(f"static/data/{email}")
+    except:
+        print("Already exists")
 
 app.run(host='0.0.0.0', port=5000,debug=True)
